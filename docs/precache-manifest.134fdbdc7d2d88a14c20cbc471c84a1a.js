40778a10b97d2fb3a65882a787cf36f0923b7921
Raúl Camacho <raulj.camacho@gmail.com>
2020-07-18 17:58:48 -0500

    Docs Agregado

---
+++ docs/precache-manifest.134fdbdc7d2d88a14c20cbc471c84a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c640b845b27624d26c7181be47f9422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93fd185b09280724fd8e",
+    "url": "/static/css/main.cc17b366.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93fd185b09280724fd8e",
+    "url": "/static/js/main.18c0b920.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
