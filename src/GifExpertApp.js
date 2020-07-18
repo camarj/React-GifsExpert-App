926486c98c8c619829536578b7f91c45e8ca9021
Raúl Camacho <raulj.camacho@gmail.com>
2020-07-18 17:57:42 -0500

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { AddCategory } from "./componentes/AddCategory";
+import { GifGrid } from "./componentes/GifGrid";
+
+export default function GifExpertApp() {
+  const [categories, setCategories] = useState(["Metallica"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+}
